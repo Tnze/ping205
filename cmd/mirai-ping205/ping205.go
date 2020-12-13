38b6b2ef962f65926318a37a3d310789e07c40fb
Tnze <cjd001113@outlook.com>
2020-12-13 16:49:21 +0800

    third commit

--- cmd/mirai-ping205/ping205.go
+++ cmd/mirai-ping205/ping205.go
@@ -1,5 +1,81 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"github.com/wangnengjie/mirai-go"
+	"github.com/wangnengjie/mirai-go/model"
+	"net"
+	"os"
+	"ping205"
+	"strings"
+)
 
+var (
+	host    = flag.String("host", "", "Hostname of mirai-api-http")
+	authKey = flag.String("authkey", "", "Auth Key")
+	Id      = flag.Int64("qq", 0, "QQ number")
+	Debug   = flag.Bool("debug", false, "Debug")
+	GroupID = flag.Int64("groupid", 0, "Group ID")
+)
+
+func main() {
+	flag.Parse()
+	bot := mirai.NewBot(mirai.BotConfig{
+		Host:      *host,
+		AuthKey:   *authKey,
+		Id:        model.QQId(*Id),
+		Websocket: false,
+		RecvMode:  mirai.RecvAll,
+		Debug:     *Debug,
+	})
+	err := bot.Connect()
+	if err != nil {
+		bot.Log.Error(err)
+	}
+	bot.On(model.GroupMessage, repeat)
+	bot.Loop()
+}
+
+func repeat(ctx *mirai.Context) {
+	m, _ := ctx.Message.(*model.GroupMsg)
+	if len(m.MessageChain) < 2 {
+		fmt.Printf("%v\n", m.MessageChain[1].(*model.Plain).Text)
+		return
+	} else if msg, ok := m.MessageChain[1].(*model.Plain); !ok {
+		return
+	} else if !strings.HasPrefix(msg.Text, "ping205") {
+		return
+	}
+
+	if m.Sender.Group.Id != model.GroupId(*GroupID) {
+		return
+	}
+	// read arp table
+	ips, err := ping205.GetArpTable()
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Get arp table error: %v\n", err)
+		os.Exit(-1)
+	}
+	var names []string
+	for _, ip := range ips {
+		names, err := net.LookupAddr(ip.String())
+		if err != nil {
+			names = append(names, ip.String())
+		} else {
+			names = append(names, strings.Join(names, "|"))
+		}
+	}
+	// 0 代表不回复消息，msgId是发出的消息的id
+	// chain中第一位为source
+	msgId, err := ctx.Bot.SendGroupMessage(m.Sender.Group.Id,
+		model.MsgChain{
+			&model.Plain{Text: strings.Join(names, "\n")},
+		}, 0)
+	// msgId 是刚刚发送的这条消息的id
+	if err != nil {
+		ctx.Bot.Log.Error(err)
+	} else {
+		ctx.Bot.Log.Info(msgId)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module ping205
 
 go 1.15
 
-require github.com/spf13/viper v1.7.1 // indirect
+require (
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/wangnengjie/mirai-go v0.2.0
+)
